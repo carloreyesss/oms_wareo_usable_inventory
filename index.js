1b4f52b90b5dffd3da7e3c15c422fbe768434a79
carloreyesss <61570450+carloreyesss@users.noreply.github.com>
2023-01-04 09:05:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
 require('dotenv').config();
 
+var _ = require('lodash');
 const fs = require('fs')  
-var request = require('request');
+var axios = require('axios');
 const csv = require('csv-parser');
 const converter = require('json-2-csv')
 
-/**
- * Read data (list of SKUs based on the given command line params)
- * 
- * @return null
- */
-var filePath = process.argv.slice(2)[0];
+;(async () => {
 
-var skus = [];
-fs.createReadStream(filePath)
-  .pipe(csv())
-  .on('data', (data) => skus.push(data.SKU))
-  .on('end', () => {
-    // call function
-    getDataAndConvertToCsv(skus.join());
-  }); 
+    // Read list of SKUs params in the CSV file
+    var filePath = process.argv.slice(2)[0];
 
-/**
- * Get data based on the lists on SKU
- * 
- * @param { } skus 
- * @return null
- */
-function getDataAndConvertToCsv(skus){
-    var options = {
-        'method': 'GET',
-        'url': 'https://ewms.anchanto.com/fetch_stock',
-        'headers': {
-            'Content-Type': 'application/json',
-            'Cookie': '_order_management_session=BAh7BkkiD3Nlc3Npb25faWQGOgZFVEkiJTBiODRiYWJiZTliOGM5ZjcwNTBmODA3NTMxM2Y5NGRhBjsAVA%3D%3D--ed0fa574134525b421d77bf16e0b220e21d8e729; locale=en'
-        },
-        body: JSON.stringify({
+    let skus = fs.readFileSync(filePath, 'utf-8');
+    skus = skus.replace(/"/g, '');
+    skus = skus.split(/\r?\n/);
+    skus.shift();
+
+    let skusChunks = _.chunk(skus, 5000); // Chunk array
+    console.log(skusChunks[0].join())
+    process.exit()
+    run(0);
+
+    async function run(i){
+        if(skusChunks[i]){
+            await getData(skusChunks[i].join(), i);
+        }
+    }
+
+    async function getData(skuLists, i){
+        var data = JSON.stringify({
             "api_key": process.env.API_KEY,
             "email": process.env.EMAIL,
             "signature": process.env.SIGNATURE,
-            "product_skus": skus
-        })
-    };
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-    
-        let results = JSON.parse(response.body);
-        let skus = [];
-     
-        results.products.forEach((val) => {
-            skus.push({
-                "Company Name": '',
-                "Product SKU": val.sku,
-                "Location": '',
-                "Qty": val.quantity
-            });
-            console.log(val)
+            "product_skus": skuLists
         });
-    
-        converter
-            .json2csvAsync(skus)
+        
+        var config = {
+            method: 'get',
+            url: 'https://ewms.anchanto.com/fetch_stock',
+            headers: { 
+            'Content-Type': 'application/json', 
+            'Cookie': '_order_management_session=BAh7BkkiD3Nlc3Npb25faWQGOgZFVEkiJTRhNzFkNjcyN2MwZmI4MjU5ZmY5MWY5ODczMjJlMzY3BjsAVA%3D%3D--b30d3a3d568bf89ff10400b21e55acb7f076eb7c; locale=en'
+            },
+            data : data
+        };
+        axios(config)
+        .then(async function (response) {
+            let results = [];
+            response.data.products.forEach( (val) => {
+
+                console.log(val.sku)
+                 results.push({
+                    "Company Name": "",
+                    "Product SKU": val.sku,
+                    "Location": "",
+                    "Qty": val.quantity
+                });
+            })
+
+            converter
+            .json2csvAsync(results)
             .then(csv => {
                 // write CSV to a file
                 let date = new Date().toISOString().slice(0, 10);
-                fs.writeFileSync(date.replace(/-/g,"") + '-' + filePath, csv)
+                if(i <= 1){
+                    fs.appendFileSync(date.replace(/-/g,"") + '-' + filePath, csv)
+                } else {
+                    fs.appendFileSync(date.replace(/-/g,"") + '-' + filePath, '\n' + csv.split('\n').slice(1).join('\n'))
+                }
             })
             .catch(err => console.log(err))
-    });
-}
+
+            i++;
+            run(i)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+       
+    }
+})();
